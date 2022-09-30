4db212d8be7f95b11e932682c7d181e749dc4a49
Red <seryprk@gmail.com>
2022-09-30 11:27:00 +0900

    feat: 프로젝트 파일 생성

---
+++ checky/checky/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  checky
+//
+//  Created by RED on 2022/09/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ checky/checky/checkyApp.swift
@@ -0,0 +1,17 @@
+//
+//  checkyApp.swift
+//  checky
+//
+//  Created by RED on 2022/09/30.
+//
+
+import SwiftUI
+
+@main
+struct checkyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
