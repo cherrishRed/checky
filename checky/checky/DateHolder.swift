16f0103c414aaae98142d2fd5aae61ee7c4c8399
Taeangel <wngks2050@gmail.com>
2022-10-03 15:08:44 +0900

    feat: DataHolder 구현

---
+++ checky/checky/DateHolder.swift
@@ -0,0 +1,12 @@
+//
+//  DateHolder.swift
+//  checky
+//
+//  Created by RED, Taeangel on 2022/10/03.
+//
+
+import Foundation
+
+class DateHolder: ObservableObject {
+   @Published var date = Date()
+}
