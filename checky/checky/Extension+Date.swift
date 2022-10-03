24e948c388d47f48f95a178b68ac2cf84b6ac619
Red <seryprk@gmail.com>
2022-10-04 02:55:26 +0900

    feat: Date extension 추가

---
+++ checky/checky/Extension+Date.swift
@@ -0,0 +1,35 @@
+//
+//  Extension+Date.swift
+//  checky
+//
+//  Created by RED, Taeangel on 2022/10/04.
+//
+
+import Foundation
+
+extension Date {
+  func getAllDates() -> [Date] {
+    
+    let calendar = Calendar.current
+    guard let startDate = calendar.date(from: Calendar.current.dateComponents([.year, .month], from: self)) else {
+      return []
+    }
+    guard let range = calendar.range(of: .day, in: .month, for: startDate) else {
+      return []
+    }
+
+    return range.compactMap { day -> Date in
+      guard let day = calendar.date(byAdding: .day, value: day - 1, to: startDate) else {
+        return Date()
+      }
+      return day
+    }
+  }
+  
+  var day: String {
+    let dateformmater = DateFormatter()
+    dateformmater.locale = Locale(identifier: "ko_KR")
+    dateformmater.dateFormat = "d"
+    return dateformmater.string(from: self)
+  }
+}
