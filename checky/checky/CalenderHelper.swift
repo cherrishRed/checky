8cfaa4f741b078265d8215e0b006f15a31b009ce
Red <seryprk@gmail.com>
2022-10-04 03:15:52 +0900

    del: CalendarHelper의 불필요한 메서드들 삭제

--- checky/checky/CalenderHelper.swift
+++ checky/checky/CalenderHelper.swift
@@ -23,24 +23,5 @@ struct CalendarHelper {
   func minusMonth(_ date: Date) -> Date {
     return calendar.date(byAdding: .month, value: -1, to: date)!
   }
-  
-  func daysInMonth(_ date: Date) -> Int {
-    let range = calendar.range(of: .day, in: .month, for: date)!
-    return range.count
-  }
-  
-  func dayOfMonth(_ date: Date) -> Int {
-    let components = calendar.dateComponents([.day], from: date)
-    return components.day!
-  }
-  
-  func firstOfMonth(_ date: Date) -> Date {
-    let components = calendar.dateComponents([.year, .month], from: date)
-    return calendar.date(from: components)!
-  }
-  
-  func weekDay(_ date: Date) -> Int {
-    let components = calendar.dateComponents([.weekday, .month], from: date)
-    return components.weekday! - 1
-  }
+
 }
