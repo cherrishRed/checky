5585d9bf3089dccbd64e21368f03e7d0442a9f43
Taeangel <wngks2050@gmail.com>
2022-10-04 22:37:00 +0900

    refactor: DateScrollerView 버튼제거

---
+++ checky/checky/View/TopView/DateScrollerView.swift
@@ -0,0 +1,33 @@
+//
+//  DateScrollerView.swift
+//  checky
+//
+//  Created by RED, Taeangel on 2022/10/03.
+//
+
+import SwiftUI
+
+struct DateScrollerView: View {
+  @EnvironmentObject var dateHolder: DateHolder
+
+    var body: some View {
+
+      HStack {
+        Spacer()
+ 
+        Text(CalendarHelper(date: dateHolder.date).monthYearString(dateHolder.date))
+          .font(.title)
+          .bold()
+          .animation(.none)
+          .frame(maxWidth: .infinity)
+        
+        Spacer()
+      }
+    }
+}
+
+struct DateScrollerView_Previews: PreviewProvider {
+    static var previews: some View {
+        DateScrollerView()
+    }
+}
