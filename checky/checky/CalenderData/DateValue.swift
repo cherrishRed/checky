0577a311e16c104e1215ee74b52d38d6f250a191
Taeangel <wngks2050@gmail.com>
2022-10-04 22:40:22 +0900

    refactor: DateValue 이니셜라이저 수정

---
+++ checky/checky/CalenderData/DateValue.swift
@@ -0,0 +1,24 @@
+//
+//  DateValue.swift
+//  checky
+//
+//  Created by RED, Taeangel on 2022/10/04.
+//
+
+import Foundation
+
+struct DateValue: Identifiable {
+  let date: Date
+  let id: String
+  let isCurrentMonth: Bool
+
+  init(
+    date: Date,
+    id: String = UUID().uuidString,
+    isCurrentMonth: Bool = true
+  ) {
+    self.date = date
+    self.id = id
+    self.isCurrentMonth = isCurrentMonth
+  }
+}
