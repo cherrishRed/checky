08fdd944625c39c24930764430654f9834ed4248
Taeangel <wngks2050@gmail.com>
2022-10-10 20:48:36 +0900

    refactor: 시작요일 변경

--- checky/checky/View/MainView/CalendarView/DayOfWeekStackView/DayOfWeekStackView.swift
+++ checky/checky/View/MainView/CalendarView/DayOfWeekStackView/DayOfWeekStackView.swift
@@ -10,12 +10,11 @@ import SwiftUI
 struct DayOfWeekStackView: View {
   @ObservedObject var viewModel: DayOfWeekStackViewModel
   
-  
   var body: some View {
     HStack(spacing: 1) {
       ForEach(viewModel.calendarHelper.startingWeek.allWeeks(), id: \.rawValue) { week in
         if viewModel.calendarHelper.weekOption == WeekOption.KoreanShort {
-          Text(week.koreanShort)
+          Text(week.short)
             .weekStyle()
         } else if viewModel.calendarHelper.weekOption == WeekOption.EnglishShort {
           Text(week.short)
