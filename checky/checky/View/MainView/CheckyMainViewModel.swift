1a562d180ba3e2c5aa3566f5c893291d1296b9a1
Taeangel <wngks2050@gmail.com>
2022-10-10 20:49:48 +0900

    feat: CheckyMainViewModel 생성

---
+++ checky/checky/View/MainView/CheckyMainViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  CheckMainView.swift
+//  checky
+//
+//  Created by RED, Taeangel on 2022/10/10.
+//
+
+import Foundation
+
+class CheckyMainViewModel: ObservableObject {
+  
+  @Published var selection: String = "home"
+  @Published var tabSelection: TabBarItem = .calendar
+  
+  init(
+    selection: String = "home",
+    tabSelection: TabBarItem = .calendar
+  ) {
+    self.selection = selection
+    self.tabSelection = tabSelection
+  }
+}
