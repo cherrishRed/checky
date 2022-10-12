bd248211e5ac60f6b5e46e9a92971a7d64df603e
Red <seryprk@gmail.com>
2022-10-13 01:16:57 +0900

    refactor: EventMenuView와 EventCreateView로 분리

--- checky/checky/View/CreateView/EventCreateView.swift
+++ checky/checky/View/CreateView/EventCreateView.swift
@@ -2,11 +2,11 @@
 //  EventCreateView.swift
 //  checky
 //
-//  Created by RED on 2022/10/11.
+//  Created by RED on 2022/10/13.
 //
 
 import SwiftUI
-import EventKit
+
 
 struct EventCreateView: View {
   @ObservedObject var viewModel = EventCreateViewModel()
@@ -14,229 +14,39 @@ struct EventCreateView: View {
   var body: some View {
     VStack {
       headerView
-        ScrollView(.vertical, showsIndicators: false) {
-          VStack(spacing: 10) {
-          titleView
-          dayAndTimePicker
-          categoryView
-          alramView
-          memoView
-        }
-      }
-      .padding(.horizontal, 10)
-    }
-    .onChange(of: viewModel.date) { newValue in
-      viewModel.changeMinimumEndDate()
+      EventMenuView(viewModel: viewModel.eventMenuViewModel)
     }
     .onTapGesture {
       hideKeyboard()
-      viewModel.closeAllPickers()
+      viewModel.tappedOutOfButton()
     }
   }
   
-    var headerView: some View {
-        HStack {
-            Button {
-              viewModel.reset()
-              hideKeyboard()
-            } label: {
-                Image(systemName: "xmark")
-                    .foregroundColor(.red)
-                    .padding()
-            }
-
-            Text("새로운 이벤트 추가")
-                .font(.title2)
-                .frame(maxWidth: .infinity)
-            
-            Button {
-              viewModel.createEvent()
-              hideKeyboard()
-            } label: {
-                Image(systemName: "checkmark")
-                    .foregroundColor(.green)
-                    .padding()
-            }
-        }
-    }
-    var titleView: some View {
-      TextField("제목", text: $viewModel.title)
-        .foregroundColor(Color("fontDarkBlack"))
-        .font(.title3)
-        .fontWeight(.bold)
-        .padding(6)
-        .frame(maxWidth: .infinity)
-        .background(Color("backgroundGray"))
-        .cornerRadius(4)
-    }
-    
-    var dayAndTimePicker: some View {
-      VStack {
-        HStack(alignment: .center) {
-          Image(systemName: "clock.fill")
-            .foregroundColor(Color("fontMediumGray"))
-            .padding(.trailing, 3)
-          if viewModel.isAllDay {
-            Button {
-              viewModel.toggleDatePicker()
-              hideKeyboard()
-            } label: {
-              Text(viewModel.date.dateKoreanWithYear)
-                .foregroundColor(Color("fontDarkBlack"))
-                .font(.title3)
-                .padding(6)
-                .frame(maxWidth: .infinity)
-                .background(Color("backgroundGray"))
-                .cornerRadius(4)
-            }
-            
-          } else {
-            HStack {
-              Button {
-                viewModel.toggleDatePicker()
-                hideKeyboard()
-              } label: {
-                VStack {
-                  Text(viewModel.date.dateKorean)
-                    .foregroundColor(Color("fontDarkBlack"))
-                  Text(viewModel.date.time)
-                    .foregroundColor(Color("fontDarkBlack"))
-                    .font(.title3)
-                }
-                .padding(4)
-                .background(Color("backgroundGray"))
-                .cornerRadius(4)
-              }
-              
-              Text("~")
-              
-              Button {
-                viewModel.toggleEndDatePicker()
-                hideKeyboard()
-              } label: {
-                VStack {
-                  Text(viewModel.endDate.dateKorean)
-                    .foregroundColor(Color("fontDarkBlack"))
-                  Text(viewModel.endDate.time)
-                    .foregroundColor(Color("fontDarkBlack"))
-                    .font(.title3)
-                }
-                .padding(4)
-                .background(Color("backgroundGray"))
-                .cornerRadius(4)
-              }
-              
-            }
-          }
+  var headerView: some View {
+      HStack {
           Button {
-            viewModel.toggleIsAllDay()
+            viewModel.tappedCloseButton()
             hideKeyboard()
           } label: {
-            HStack {
-              Image(systemName: viewModel.isAllDay ? "checkmark.square.fill" : "square")
-              Text("하루종일")
-            }
-            .foregroundColor(Color("fontDarkBlack"))
+              Image(systemName: "xmark")
+                  .foregroundColor(.red)
+                  .padding()
           }
-        }
-        
-        if viewModel.isShowDatePicker {
-          
-          DatePicker("", selection: $viewModel.date,
-                     displayedComponents: viewModel.isAllDay ? [.date] : [.date, .hourAndMinute])
-            .datePickerStyle(.wheel)
-        }
-        if viewModel.isShowEndDatePicker {
-          DatePicker("", selection: $viewModel.endDate, in: viewModel.dateRange)
-            .datePickerStyle(.wheel)
-        }
-        }
-    }
-  
-  var categoryView: some View {
-    VStack {
-      HStack {
-        Image(systemName: "tag.fill")
-          .foregroundColor(Color("fontMediumGray"))
-        Button {
-          viewModel.toggleCategoriesPicker()
-          hideKeyboard()
-        } label: {
-          HStack {
-            Circle()
-              .fill(Color(cgColor: viewModel.category.cgColor))
-              .frame(width: 10, height: 10)
-            Text(viewModel.category.title)
-              .foregroundColor(Color("fontDarkBlack"))
-              .font(.title3)
-          }
-          .padding(4)
-          .frame(maxWidth: .infinity)
-          .background(Color("backgroundGray"))
-          .cornerRadius(4)
-        }
-      }
-      
-      if viewModel.isShowCategoriesPicker {
-        Picker("", selection: $viewModel.category) {
-          ForEach(viewModel.categories) { cate in
-            HStack {
-              Circle()
-                .fill(Color(cgColor: cate.cgColor))
-                .frame(width: 10, height: 10)
-              Text(cate.title)
-            }.tag(cate)
-          }
-        }
-        .pickerStyle(WheelPickerStyle())
-      }
-    }
-  }
-  
-  var alramView: some View {
-    VStack {
-      HStack() {
-        Image(systemName: "alarm.fill")
-          .foregroundColor(Color("fontMediumGray"))
-          .padding(.trailing, 4)
-        Button {
-          viewModel.toggleAlramPicker()
-          hideKeyboard()
-        } label: {
-          Text(viewModel.alram.korean)
-            .foregroundColor(Color("fontDarkBlack"))
-            .font(.title3)
-            .padding(4)
-            .frame(maxWidth: .infinity)
-            .background(Color("backgroundGray"))
-            .cornerRadius(4)
-        }
-      }
-      if viewModel.isShowAlramPicker {
-        Picker("", selection: $viewModel.alram) {
-          ForEach(AlramTime.allCases, id: \.self) { alram in
-            Text(alram.korean).tag(alram)
-          }
-        }
-        .pickerStyle(WheelPickerStyle())
-      }
-    }
-  }
-  
-  var memoView: some View {
-    HStack(alignment: .top) {
-      Image(systemName: "note.text")
-        .foregroundColor(Color("fontMediumGray"))
-        .padding(.top, 10)
-        .padding(.trailing, 2)
-      TextField("메모", text: $viewModel.memo, axis: .vertical)
-        .foregroundColor(Color("fontDarkBlack"))
-        .font(.title3)
-        .padding(6)
-        .frame(maxWidth: .infinity)
-        .background(Color("backgroundGray"))
-        .cornerRadius(4)
-    }
-  }
-}
 
+          Text("새로운 이벤트 추가")
+              .font(.title2)
+              .frame(maxWidth: .infinity)
+          
+          Button {
+            viewModel.tappedCreateButton()
+            hideKeyboard()
+          } label: {
+              Image(systemName: "checkmark")
+                  .foregroundColor(.green)
+                  .padding()
+          }
+      }
+  }
+  
+}
+      
---
+++ checky/checky/View/CreateView/EventMenuViewModel.swift
@@ -0,0 +1,134 @@
+//
+//  EventMenuViewModel.swift
+//  checky
+//
+//  Created by RED on 2022/10/12.
+//
+
+import Foundation
+import EventKit
+
+class EventMenuViewModel: ObservableObject {
+  let eventManager = EventManager()
+  let categories: [EKCalendar]
+  
+  // 제목
+  @Published var title: String = ""
+  
+  // 날짜
+  @Published var date: Date = .now
+  @Published var endDate: Date = .now
+  @Published var isShowDatePicker: Bool = false
+  @Published var isShowEndDatePicker: Bool = false
+  @Published var isAllDay: Bool = false
+  
+  // 카테고리
+  @Published var category: EKCalendar
+  @Published var isShowCategoriesPicker: Bool = false
+  
+  // 메모
+  @Published var memo: String = ""
+  
+  //
+  @Published var alram: AlramTime = .none
+  @Published var isShowAlramPicker: Bool = false
+  
+  
+  init() {
+    self.categories = eventManager.getEventCategories()
+    self.category = categories[0]
+  }
+  
+  var dateRange: ClosedRange<Date> {
+    let max = Calendar.current.date(
+         byAdding: .year,
+         value: 10,
+         to: date
+       ) ?? Date()
+    return date...max
+  }
+  
+  func toggleDatePicker() {
+    if isShowDatePicker == true {
+      isShowDatePicker = false
+    } else {
+      closeAllPickers()
+      isShowDatePicker = true
+    }
+  }
+  
+  func toggleEndDatePicker() {
+    if isShowEndDatePicker == true {
+      isShowEndDatePicker = false
+    } else {
+      closeAllPickers()
+      isShowEndDatePicker = true
+    }
+  }
+  
+  func toggleCategoriesPicker() {
+    if isShowCategoriesPicker == true {
+      isShowCategoriesPicker = false
+    } else {
+      closeAllPickers()
+      isShowCategoriesPicker = true
+    }
+  }
+  
+  func toggleAlramPicker() {
+    if isShowAlramPicker == true {
+      isShowAlramPicker = false
+    } else {
+      closeAllPickers()
+      isShowAlramPicker = true
+    }
+  }
+  
+  func toggleIsAllDay() {
+    isAllDay.toggle()
+  }
+  
+  func closeAllPickers() {
+    isShowCategoriesPicker = false
+    isShowDatePicker = false
+    isShowEndDatePicker = false
+    isShowAlramPicker = false
+  }
+  
+  func createEvent() {
+    let newEvnet = EKEvent(eventStore: eventManager.store)
+    newEvnet.title = title
+    newEvnet.isAllDay = isAllDay
+    newEvnet.startDate = date
+    newEvnet.calendar = category
+    newEvnet.notes = memo
+    
+    if isAllDay == false {
+      newEvnet.endDate = endDate
+    } else {
+      newEvnet.endDate = date
+    }
+    
+    if alram != .none {
+      newEvnet.addAlarm(EKAlarm(relativeOffset: alram.second))
+    }
+    
+    eventManager.createNewEvent(newEvent: newEvnet)
+    reset()
+  }
+  
+  func reset() {
+    title = ""
+    isAllDay = false
+    date = .now
+    endDate = .now
+    memo = ""
+    category = categories[0]
+  }
+  
+  func changeMinimumEndDate() {
+    if endDate < date {
+      endDate = date
+    }
+  }
+}
