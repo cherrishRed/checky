c6b114f1803abbae46c84f88eb935427dc3ab50c
Taeangel <wngks2050@gmail.com>
2022-10-13 19:08:21 +0900

    feat: WeeklyView 생성

---
+++ checky/checky/View/MainView/CalendarView/WeeklyView/WeeklyView.swift
@@ -0,0 +1,41 @@
+//
+//  WeeklyView.swift
+//  checky
+//
+//  Created by song on 2022/10/12.
+//
+
+import SwiftUI
+
+struct WeeklyView: View {
+  @ObservedObject var viewModel: WeeklyViewModel
+  
+  var body: some View {
+    CalendarGrid
+      .onAppear {
+        viewModel.getPermission()
+        viewModel.fetchEvents()
+        viewModel.fetchReminder()
+      }
+  }
+  
+  var CalendarGrid: some View {
+    let columns = Array(repeating: GridItem(.flexible(), spacing: 0, alignment: nil), count: 2)
+    let columnsCount: CGFloat = 8
+    
+    var body: some View {
+      GeometryReader { geo in
+        LazyVGrid(columns: columns, spacing: 0) {
+          ForEach(viewModel.allDatesForDisplay) { value in
+            WeeklyCellView(dateValue: value, allEvnets: viewModel.filteredEvent(value.date), allReminders: viewModel.filteredReminder(value.date))
+              .frame(width: geo.size.width / 2, height: geo.size.height / columnsCount * 2)
+          }
+        }
+      }
+      .frame(maxHeight: .infinity)
+    }
+    return body
+  }
+}
+
+
