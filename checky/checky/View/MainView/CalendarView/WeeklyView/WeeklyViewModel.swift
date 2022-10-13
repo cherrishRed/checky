b051e8772cc9e21d91f8ba6df345afeac809fe30
Taeangel <wngks2050@gmail.com>
2022-10-13 19:08:38 +0900

    feat: WeeklyViewModel 생성

---
+++ checky/checky/View/MainView/CalendarView/WeeklyView/WeeklyViewModel.swift
@@ -0,0 +1,59 @@
+//
+//  WeeklyViewModel.swift
+//  checky
+//
+//  Created by song on 2022/10/12.
+//
+
+import Foundation
+
+class WeeklyViewModel: ObservableObject {
+  @Published var dateHolder: DateHolder
+  @Published var events: [Event]
+  @Published var reminders: [Reminder]
+  let eventManager: EventManager
+  let calendarHelper: CalendarHelper
+  
+  init(dateHolder: DateHolder,
+       eventManager: EventManager,
+       calendarHelper: CalendarHelper,
+       events: [Event] = [],
+       reminders: [Reminder] = []
+  ) {
+    self.dateHolder = dateHolder
+    self.eventManager = eventManager
+    self.calendarHelper = calendarHelper
+    self.events = events
+    self.reminders = reminders
+  }
+  
+  func getPermission() {
+    eventManager.getPermission()
+  }
+  
+  func fetchEvents() {
+    events = eventManager.getAllEventforThisMonth(date: dateHolder.date)
+  }
+
+  func fetchReminder() {
+    eventManager.getAllReminderforThisMonth(date: dateHolder.date, completionHandler: { [weak self] reminderList in
+      DispatchQueue.main.async {
+        self?.reminders = reminderList
+      }
+    })
+  }
+  
+  
+  var allDatesForDisplay: [DateValue] {
+    return calendarHelper.extractWeekDates(dateHolder.date)
+  }
+ 
+  
+  func filteredEvent(_ date: Date) -> [Event] {
+    eventManager.filterEvent(events, date)
+  }
+  
+  func filteredReminder(_ date: Date) -> [Reminder] {
+    eventManager.filterReminder(reminders, date)
+  }
+}
