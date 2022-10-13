35a30e5bd876c38b3972f0da81787b8a42ec994f
Taeangel <wngks2050@gmail.com>
2022-10-13 19:08:06 +0900

    feat: WeeklyCellView 생성

---
+++ checky/checky/View/MainView/CalendarView/MonthlyView/DayOfWeekStackView/DayOfWeekStackView.swift
@@ -0,0 +1,32 @@
+//
+//  CalendarGridView.swift
+//  checky
+//
+//  Created by song on 2022/10/06.
+//
+
+import SwiftUI
+
+struct DayOfWeekStackView: View {
+  @ObservedObject var viewModel: DayOfWeekStackViewModel
+  
+  var body: some View {
+    VStack(spacing: 4) {
+      HStack(spacing: 1) {
+        ForEach(viewModel.calendarHelper.startingWeek.allWeeks(), id: \.rawValue) { week in
+          if viewModel.calendarHelper.weekOption == WeekOption.KoreanShort {
+            Text(week.short)
+              .weekStyle()
+          } else if viewModel.calendarHelper.weekOption == WeekOption.EnglishShort {
+            Text(week.short)
+              .weekStyle()
+          } else {
+            Text("\(week.rawValue)")
+              .weekStyle()
+          }
+        }
+      }
+      Divider()
+    }
+  }
+}
---
+++ checky/checky/View/MainView/CalendarView/WeeklyView/WeeklyCellView.swift
@@ -0,0 +1,94 @@
+//
+//  WeeklyCellView.swift
+//  checky
+//
+//  Created by song on 2022/10/12.
+//
+
+import SwiftUI
+
+struct WeeklyCellView: View {
+  @State var dateValue: DateValue
+  @State var allEvnets: [Event] = []
+  @State var allReminders: [Reminder] = []
+  
+  var body: some View {
+    VStack(alignment: .trailing) {
+      HStack {
+        Text(dateValue.date.dayOfWeek)
+          .foregroundColor(.black.opacity(0.5))
+        Text(dateValue.date.day)
+      }
+      .frame(alignment: .trailing)
+      
+      ForEach(allEvnets, id: \.self) { event in
+        WeeklyEventBlockView(event: event)
+      }
+      .padding(10)
+      .frame(maxWidth: .infinity)
+      
+      ForEach(allReminders, id: \.self) { reminder in
+        ReminderBlockView(reminder: reminder)
+      }
+      .frame(maxWidth: .infinity)
+    }
+  }
+}
+
+struct WeeklyEventBlockView: View {
+  let event: Event
+  
+  var body: some View {
+    ZStack(alignment: .leading) {
+      Rectangle()
+        .fill(Color(event.category.cgColor))
+        .frame(maxWidth: .infinity)
+        .layoutPriority(1)
+      
+      Text(event.ekevent.title)
+        .padding(1)
+        .lineLimit(1)
+        .font(.caption)
+        .fontWeight(.semibold)
+        .foregroundColor(Color("basicWhite"))
+        .fixedSize(horizontal: true, vertical: false)
+    }
+    .frame(height: 16)
+    .padding(.vertical, 1.1)
+  }
+}
+
+struct WeeklyReminderBlockView: View {
+  let reminder: Reminder
+  
+  var body: some View {
+    HStack(spacing: 1) {
+      ZStack {
+        Circle()
+          .stroke(Color(reminder.category.cgColor))
+          .frame(width: 10 , height: 10)
+        Circle()
+          .fill(Color(reminder.category.cgColor))
+          .frame(width: 6 , height: 6)
+          .opacity(reminder.ekreminder.isCompleted ? 1.0 : 0.0)
+      }
+      
+      ZStack(alignment: .leading) {
+        Rectangle()
+          .fill(Color("basicWhite"))
+          .frame(maxWidth: .infinity)
+          .layoutPriority(1)
+        Text(reminder.ekreminder.title)
+          .font(.caption)
+          .foregroundColor(Color("fontDarkBlack"))
+          .fixedSize(horizontal: true, vertical: false)
+        Text(reminder.ekreminder.description)
+          .font(.caption)
+          .foregroundColor(Color("fontDarkBlack"))
+          .fixedSize(horizontal: true, vertical: false)
+      }
+    }
+    .frame(height: 16)
+    .padding(.horizontal, 2)
+  }
+}
