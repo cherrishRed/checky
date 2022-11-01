06d23013ac47b10e586d0419faa954619848ab5a
Taeangel <wngks2050@gmail.com>
2022-11-01 16:57:29 +0900

    feat: EventSettingViewModel 생성

---
+++ checky/checky/View/Setting/Event/EventSettingViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  EventSettingViewModel.swift
+//  checky
+//
+//  Created by song on 2022/11/01.
+//
+
+import Foundation
+import EventKit
+
+class EventSettingViewModel: ObservableObject {
+  let eventManager: EventManager
+  @Published var categories: [EKCalendar]
+  init(
+    eventManager: EventManager
+  ) {
+    self.eventManager = eventManager
+    self.categories = eventManager.getTaskCategories()
+  }
+}
