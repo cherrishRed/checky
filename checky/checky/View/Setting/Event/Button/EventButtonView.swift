132b60eb672066f377ba0c556bb97a843967fd6c
Taeangel <wngks2050@gmail.com>
2022-11-02 17:20:06 +0900

    feat: EventButtonView 구성

--- checky/checky/View/Setting/Event/Button/EventButtonView.swift
+++ checky/checky/View/Setting/Event/Button/EventButtonView.swift
@@ -10,40 +10,59 @@ import EventKit
 
 struct EventButtonView: View {
   @ObservedObject var viewModel: EventButtonViewModel
-  @State var Imoji: String?
   
   var body: some View {
+    
     HStack {
-      Text("이모지")
-      Spacer()
-      Text(viewModel.imoji ?? "🐲")
+      VStack(spacing: 30) {
+        ZStack {
+          Rectangle()
+            .fill(Color.white)
+            .frame(height: 30)
+            .frame(maxWidth: .infinity)
+          
+          HStack {
+            Text("이모지")
+            Spacer()
+            Text(viewModel.imoji)
+          }
+          .padding(.horizontal, 15)
+        }
+        
+        ZStack {
+          Rectangle()
+            .fill(Color.white)
+            .frame(height: 30)
+            .frame(maxWidth: .infinity)
+          
+          HStack {
+            Text("색상")
+            Spacer()
+            
+            Rectangle()
+              .fill(viewModel.color)
+              .frame(width: 15, height: 15)
+            
+          }
+          .padding(.horizontal, 17)
+        }
+        Spacer()
+        ColorView(color: $viewModel.color, calendarIdentifier: viewModel.category.calendarIdentifier)
+        EmojiView(txt: $viewModel.imoji, calendarIdentifier: viewModel.category.calendarIdentifier)
+        Spacer()
+      }
+      .padding(.horizontal, 15)
     }
     .onAppear {
-      viewModel.imoji = UserDefaults.standard.string(forKey: viewModel.category.calendarIdentifier)
+      viewModel.imoji = UserDefaults.standard.string(forKey: ("\(viewModel.category.calendarIdentifier)_imoji")) ?? ""
+      
+      guard let colorComponent = UserDefaults.standard.object(forKey: ("\(viewModel.category.calendarIdentifier)_color")) as? [CGFloat] else {
+        return
+      }
+      
+      viewModel.color = Color(.sRGB, red: colorComponent[0], green: colorComponent[1], blue: colorComponent[2], opacity: colorComponent[3])
     }
+    .background(Color.backgroundGray)
   }
 }
 
-//struct ContentView: View {
-//
-//  @State var currentUserName: String?
-//
-//  var body: some View {
-//    VStack(spacing: 20) {
-//      Text(currentUserName ?? "Add Name Here")
-//
-//      if let name = currentUserName {
-//        Text(name)
-//      }
-//
-//      Button("Save".uppercased()) {
-//        let name = "Song"
-//        currentUserName = name
-//        UserDefaults.standard.set(name, forKey: viewModel.category.calendarIdentifier)
-//      }
-//    }
-//    .onAppear {
-//      currentUserName = UserDefaults.standard.string(forKey: viewModel.category.calendarIdentifier)
-//    }
-//  }
-//}
