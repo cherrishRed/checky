5a810c4fcb1fa5097bf0c4ad6bf929ba427fb375
Taeangel <wngks2050@gmail.com>
2022-11-02 17:16:25 +0900

    feat: BasicColor 생성

--- checky/checky/Extension/Extension+Color.swift
+++ checky/checky/Extension/Extension+Color.swift
@@ -16,6 +16,19 @@ extension Color {
   static let fontDarkBlack = Color("fontDarkBlack")
   static let fontLightGray = Color("fontLightGray")
   static let fontMediumGray = Color("fontMediumGray")
-  
   static let pointRed = Color("pointRed")
+}
+
+// MARK: - BasicColor
+extension Color {
+  static let basicRed = Color("BasicRed")
+  static let basicOrange = Color("BasicOrange")
+  static let basicYellow = Color("BasicYellow")
+  static let basicLightGreen = Color("BasicLightGreen")
+  static let basicGreen = Color("BasicGreen")
+  static let basicSkyblue = Color("BasicSkyblue")
+  static let basicLightIndigo = Color("BasicLightIndigo")
+  static let basicIndigo = Color("BasicIndigo")
+  static let basicPink = Color("BasicPink")
+  static let basicPuple = Color("BasicPurple")
 }
