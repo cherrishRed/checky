27d7f77e6c7efa70f180d2643697cdc42a376acf
Taeangel <wngks2050@gmail.com>
2022-11-02 12:11:45 +0900

    refactor: ReminderSettingView 수정

--- checky/checky/View/Setting/Reminder/ReminderSettingView.swift
+++ checky/checky/View/Setting/Reminder/ReminderSettingView.swift
@@ -10,23 +10,41 @@ import SwiftUI
 struct ReminderSettingView: View {
   @ObservedObject var viewModel: ReminderSettingViewModel
   @EnvironmentObject var coordinator: Coordinator<checkyRouter>
+  
+  var body: some View {
+    
+    ScrollView {
+      VStack(alignment: .leading) {
+        ForEach(viewModel.categories) { category in
+          
+          Button(action: {
+            coordinator.show(.ReminderSettingButton(category: category))
+          }, label: {
+            ZStack {
+              Rectangle()
+                .fill(Color.white)
+                .frame(height: 30)
+                .frame(maxWidth: .infinity)
 
-    var body: some View {
-      
-      ForEach(viewModel.categories) { category in
-        Button(action: {
-          coordinator.show(.ReminderSettingButton(category: category))
-        }, label: {
-          HStack {
-            Circle()
-              .fill(Color(cgColor: category.cgColor))
-              .frame(width: 10, height: 10)
-            Text(category.title)
-              .foregroundColor(Color.fontDarkBlack)
-              .font(.title3)
-          }
-        })
+              HStack {
+                Text(category.title)
+                  .foregroundColor(Color.fontDarkBlack)
+                  .font(.title3)
+                
+                Spacer()
+                
+                Rectangle()
+                  .fill(Color(cgColor: category.cgColor))
+                  .frame(width: 10, height: 10)
+               }
+              .padding(.horizontal)
+            }
+            .frame(alignment: .top)
+          })
+        }
       }
+      .padding(.horizontal, 15)
     }
+    .background(Color.backgroundGray)
+  }
 }
-   
