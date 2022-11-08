0eb04b84dcf712d3915f797a7ea79755ee884907
Taeangel <wngks2050@gmail.com>
2022-11-08 11:44:38 +0900

    refactor: SettingListView 수정 관련 파일 정리

--- checky/checky/Coordinator/CheckyRouter.swift
+++ checky/checky/Coordinator/CheckyRouter.swift
@@ -16,31 +16,25 @@ enum checkyRouter: NavigationRouter {
   case daily(Date, [Event], [Reminder], [Reminder], EventManager, ReminderManager)
   case editEvent(Event, EventManager)
   case editReminder(Reminder, ReminderManager)
-  case eventSetting
-  case reminderSetting
   case eventSettingButton(category: EKCalendar)
   case reminderSettingButton(category: EKCalendar)
   
   var transition: NavigationTranisitionStyle {
     switch self {
-      case .main:
-        return .push
-      case .create:
-        return .presentHalfModally
-      case .createEvent:
-        return .presentModally
-      case .createReminder:
-        return .presentModally
-      case .daily:
-        return .push
-      case .editEvent:
-        return .presentModally
-      case .editReminder:
-        return .presentModally
-       case .eventSetting:
+    case .main:
       return .push
-    case .reminderSetting:
+    case .create:
+      return .presentHalfModally
+    case .createEvent:
+      return .presentModally
+    case .createReminder:
+      return .presentModally
+    case .daily:
       return .push
+    case .editEvent:
+      return .presentModally
+    case .editReminder:
+      return .presentModally
     case .eventSettingButton:
       return .push
     case .reminderSettingButton:
@@ -50,34 +44,26 @@ enum checkyRouter: NavigationRouter {
   
   @ViewBuilder
   func view() -> some View {
-    
-    let eventManager = EventManager()
-    let reminderManager = ReminderManager()
     switch self {
-
-      case .main:
-        checkyApp()
-      case .create:
-        CreateSelectorView()
-      case .createEvent:
-        EventCreateAndEditView(viewModel: EventCreateAndEditViewModel(mode: .create, eventManager: EventManager()))
-      case .createReminder:
-        ReminderCreateAndEditView(viewModel: ReminderCreateAndEditViewModel(mode: .create, reminderManager: ReminderManager()))
-      case let .daily(date, events, reminders, clearedReminders, eventManager, reminderManager):
-        DailyView(viewModel: DailyViewModel(date: date, events: events, reminders: reminders, clearedReminders: clearedReminders, eventManager: eventManager, reminderManager: reminderManager))
-      case let .editEvent(event, eventManager):
-        EventCreateAndEditView(viewModel: EventCreateAndEditViewModel(event: event, eventManager: eventManager))
-      case let .editReminder(reminder, reminderManager):
-        ReminderCreateAndEditView(viewModel: ReminderCreateAndEditViewModel(reminder: reminder, reminderManager: reminderManager))
-      case .eventSetting:
-      EventCategoriListView(viewModel: EventCategoriListViewModel(eventManager: eventManager))
-    case .reminderSetting:
-      ReminderCategoriListView(viewModel: ReminderCategoriListViewModel(reminderManager: reminderManager))
+    case .main:
+      checkyApp()
+    case .create:
+      CreateSelectorView()
+    case .createEvent:
+      EventCreateAndEditView(viewModel: EventCreateAndEditViewModel(mode: .create, eventManager: EventManager()))
+    case .createReminder:
+      ReminderCreateAndEditView(viewModel: ReminderCreateAndEditViewModel(mode: .create, reminderManager: ReminderManager()))
+    case let .daily(date, events, reminders, clearedReminders, eventManager, reminderManager):
+      DailyView(viewModel: DailyViewModel(date: date, events: events, reminders: reminders, clearedReminders: clearedReminders, eventManager: eventManager, reminderManager: reminderManager))
+    case let .editEvent(event, eventManager):
+      EventCreateAndEditView(viewModel: EventCreateAndEditViewModel(event: event, eventManager: eventManager))
+    case let .editReminder(reminder, reminderManager):
+      ReminderCreateAndEditView(viewModel: ReminderCreateAndEditViewModel(reminder: reminder, reminderManager: reminderManager))
     case let .eventSettingButton(category):
-      EventCategoriSettingView(viewModel: EventCategoriSettingViewModel(category: category, eventManager: eventManager))
+      EventCategoriSettingView(viewModel: EventCategoriSettingViewModel(category: category, eventManager: EventManager()))
     case let .reminderSettingButton(category):
-      ReminderCategoriSettingView(viewModel: ReminderCategoriSettingViewModel(category: category, reminderManager: reminderManager))
-
+      ReminderCategoriSettingView(viewModel: ReminderCategoriSettingViewModel(category: category, reminderManager: ReminderManager()))
+      
     }
   }
 }
