8214d5fb8de93fade7e0b9b016cace15acb9bba4
Red <seryprk@gmail.com>
2022-11-15 22:21:16 +0900

    refactor: widget 추가

--- checky/checkyWidget/checkyWidget.swift
+++ checky/checkyWidget/checkyWidget.swift
@@ -8,62 +8,127 @@
 import WidgetKit
 import SwiftUI
 import Intents
+import EventKit
 
 struct Provider: IntentTimelineProvider {
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), configuration: ConfigurationIntent())
+  let eventManager = EventManagerForWidget()
+  
+  func placeholder(in context: Context) -> SimpleEntry {
+    SimpleEntry(date: Date(), events: [])
+  }
+  
+  func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+    let entry = SimpleEntry(date: Date(), events: eventManager.getAllTaskforToday())
+    completion(entry)
+  }
+  
+  func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+    var entries: [SimpleEntry] = []
+    
+    let currentDate = Date()
+    for hourOffset in 0 ..< 5 {
+      let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+      let entry = SimpleEntry(date: entryDate, events: eventManager.getAllTaskforToday())
+      entries.append(entry)
     }
-
-    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), configuration: configuration)
-        completion(entry)
-    }
-
-    func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
-
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, configuration: configuration)
-            entries.append(entry)
-        }
-
-        let timeline = Timeline(entries: entries, policy: .atEnd)
-        completion(timeline)
-    }
+    
+    let timeline = Timeline(entries: entries, policy: .atEnd)
+    completion(timeline)
+  }
 }
 
 struct SimpleEntry: TimelineEntry {
-    let date: Date
-    let configuration: ConfigurationIntent
+  let date: Date
+  let events: [EKEvent]
 }
 
-struct checkyWidgetEntryView : View {
-    var entry: Provider.Entry
-
-    var body: some View {
-        Text(entry.date, style: .time)
+struct calendarWidgetEntryView : View {
+  var entry: Provider.Entry
+  
+  var body: some View {
+    VStack {
+      Text("오늘은 \(Date().month)월 \(Date().day)일")
+        .fontWeight(.bold)
+        .foregroundColor(Color.fontBlack)
+      VStack(spacing: 4) {
+        ForEach(entry.events, id: \.eventIdentifier) { event in
+          ZStack(alignment: .leading) {
+            RoundedRectangle(cornerRadius: 4)
+              .fill(Color(cgColor: event.calendar.cgColor))
+            Text(event.title)
+              .foregroundColor(Color.basicWhite)
+              .fontWeight(.bold)
+              .padding(.horizontal, 4)
+          }
+          .fixedSize(horizontal: false, vertical: true)
+        }
+      }
+      Spacer()
     }
+    .padding(.vertical, 10)
+    .padding(.horizontal, 10)
+  }
 }
 
 @main
-struct checkyWidget: Widget {
-    let kind: String = "checkyWidget"
-
-    var body: some WidgetConfiguration {
-        IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
-            checkyWidgetEntryView(entry: entry)
-        }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
+struct calendarWidget: Widget {
+  let kind: String = "calendarWidget"
+  
+  var body: some WidgetConfiguration {
+    IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
+      calendarWidgetEntryView(entry: entry)
     }
+    .configurationDisplayName("오늘 목록")
+    .description("오늘 할일 목록을 보여주는 위젯입니다")
+    .supportedFamilies([.systemSmall])
+  }
 }
 
-struct checkyWidget_Previews: PreviewProvider {
-    static var previews: some View {
-        checkyWidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
-            .previewContext(WidgetPreviewContext(family: .systemSmall))
-    }
+struct calendarWidget_Previews: PreviewProvider {
+  static var previews: some View {
+    calendarWidgetEntryView(entry: SimpleEntry(date: Date(), events: []))
+      .previewContext(WidgetPreviewContext(family: .systemSmall))
+  }
 }
+
+
+
+
+struct EventManagerForWidget  {
+  var store: EKEventStore
+  var calendar: Calendar
+
+  init(store: EKEventStore = EKEventStore(),
+       calendar: Calendar = Calendar(identifier: .gregorian)) {
+    self.store = store
+    self.calendar = calendar
+  }
+  
+  // 오늘 이벤트 받아 오는 메서드
+  func getAllTaskforToday(date: Date = Date()) -> [EKEvent] {
+    
+    let categories = store.calendars(for: .event)
+    
+    var list: [EKEvent] = []
+    
+    for category in categories {
+      let predicate = store.predicateForEvents(withStart: date, end: date, calendars: [category])
+      let eventList = store.events(matching: predicate)
+      list.append(contentsOf: eventList)
+    }
+    
+    return list
+  }
+  
+}
+
+extension View {
+  func fetchUserDefaultColor(calendarIdentifier: String) -> Color {
+    guard let colorComponent = UserDefaults.standard.object(forKey: ("\(calendarIdentifier)_color")) as? [CGFloat] else {
+      return Color.white
+    }
+    
+   return  Color(.sRGB, red: colorComponent[0], green: colorComponent[1], blue: colorComponent[2], opacity: colorComponent[3])
+  }
+}
+
