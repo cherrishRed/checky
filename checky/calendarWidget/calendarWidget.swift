f40b231849e74c4feddb1835607d1da0ad96a268
Red <seryprk@gmail.com>
2022-11-15 20:40:21 +0900

    feat: today widget 생성

--- checky/calendarWidget/calendarWidget.swift
+++ checky/calendarWidget/calendarWidget.swift
@@ -8,14 +8,17 @@
 import WidgetKit
 import SwiftUI
 import Intents
+import EventKit
 
 struct Provider: IntentTimelineProvider {
+  let eventManager = EventManagerForWidget()
+  
   func placeholder(in context: Context) -> SimpleEntry {
-    SimpleEntry(date: Date())
+    SimpleEntry(date: Date(), events: [])
   }
   
   func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-    let entry = SimpleEntry(date: Date())
+    let entry = SimpleEntry(date: Date(), events: eventManager.getAllTaskforToday())
     completion(entry)
   }
   
@@ -25,7 +28,7 @@ struct Provider: IntentTimelineProvider {
     let currentDate = Date()
     for hourOffset in 0 ..< 5 {
       let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-      let entry = SimpleEntry(date: entryDate)
+      let entry = SimpleEntry(date: entryDate, events: eventManager.getAllTaskforToday())
       entries.append(entry)
     }
     
@@ -36,13 +39,34 @@ struct Provider: IntentTimelineProvider {
 
 struct SimpleEntry: TimelineEntry {
   let date: Date
+  let events: [EKEvent]
 }
 
 struct calendarWidgetEntryView : View {
   var entry: Provider.Entry
   
   var body: some View {
-    Text(entry.date, style: .time)
+    VStack {
+      Text("오늘은 \(Date().month)월 \(Date().day)일")
+        .fontWeight(.bold)
+        .foregroundColor(Color.basicGreen)
+      VStack(spacing: 4) {
+        ForEach(entry.events, id: \.eventIdentifier) { event in
+          ZStack(alignment: .leading) {
+            RoundedRectangle(cornerRadius: 4)
+              .fill(Color(cgColor: event.calendar.cgColor))
+            Text(event.title)
+              .foregroundColor(Color.basicWhite)
+              .fontWeight(.bold)
+              .padding(.horizontal, 4)
+          }
+          .fixedSize(horizontal: false, vertical: true)
+        }
+      }
+      Spacer()
+    }
+    .padding(.vertical, 10)
+    .padding(.horizontal, 10)
   }
 }
 
@@ -54,14 +78,55 @@ struct calendarWidget: Widget {
     IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
       calendarWidgetEntryView(entry: entry)
     }
-    .configurationDisplayName("My Widget")
+    .configurationDisplayName("오늘 목록")
     .description("This is an example widget.")
   }
 }
 
 struct calendarWidget_Previews: PreviewProvider {
   static var previews: some View {
-    calendarWidgetEntryView(entry: SimpleEntry(date: Date()))
+    calendarWidgetEntryView(entry: SimpleEntry(date: Date(), events: []))
       .previewContext(WidgetPreviewContext(family: .systemSmall))
+  }
+}
+
+
+
+
+struct EventManagerForWidget  {
+  var store: EKEventStore
+  var calendar: Calendar
+
+  init(store: EKEventStore = EKEventStore(),
+       calendar: Calendar = Calendar(identifier: .gregorian)) {
+    self.store = store
+    self.calendar = calendar
+  }
+  
+  // 오늘 이벤트 받아 오는 메서드
+  func getAllTaskforToday(date: Date = Date()) -> [EKEvent] {
+    
+    let categories = store.calendars(for: .event)
+    
+    var list: [EKEvent] = []
+    
+    for category in categories {
+      let predicate = store.predicateForEvents(withStart: date, end: date, calendars: [category])
+      let eventList = store.events(matching: predicate)
+      list.append(contentsOf: eventList)
+    }
+    
+    return list
+  }
+  
+}
+
+extension View {
+  func fetchUserDefaultColor(calendarIdentifier: String) -> Color {
+    guard let colorComponent = UserDefaults.standard.object(forKey: ("\(calendarIdentifier)_color")) as? [CGFloat] else {
+      return Color.white
+    }
+    
+   return  Color(.sRGB, red: colorComponent[0], green: colorComponent[1], blue: colorComponent[2], opacity: colorComponent[3])
   }
 }
